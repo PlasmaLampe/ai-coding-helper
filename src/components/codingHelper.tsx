f22beb9e4af30b028016e3c8cb02a26db2fb33dc
tech <tech@myfractalrange.com>
2023-01-17 12:52:48 +0100

    Improves styles

--- src/components/codingHelper.tsx
+++ src/components/codingHelper.tsx
@@ -1,69 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { PrompterService } from "../services/prompter";
 
 interface Props {
-    // any other props that this component might need
+	// any other props that this component might need
 }
 
 export const CoderForm: React.FC<Props> = () => {
-    const [command, setCommand] = useState('');
-    const [context, setContext] = useState('');
-    const [addTests, setAddTests] = useState(false);
-    const [addDocumentation, setAddDocumentation] = useState(true);
-    const [fastReviewMode, setFastReviewMode] = useState(false);
-    const [out, setOut] = useState('');
+	const [command, setCommand] = useState("");
+	const [context, setContext] = useState("");
+	const [addTests, setAddTests] = useState(false);
+	const [addDocumentation, setAddDocumentation] = useState(true);
+	const [fastReviewMode, setFastReviewMode] = useState(false);
+	const [out, setOut] = useState("");
 
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        const prompt = PrompterService.createPrompt({
-            command,
-            context,
-            addTests,
-            addDocumentation,
-            fastReviewMode
-        });
-        setOut(prompt);
-    }
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault();
+		const prompt = PrompterService.createPrompt({
+			command,
+			context,
+			addTests,
+			addDocumentation,
+			fastReviewMode
+		});
+		setOut(prompt);
+	};
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div className="form-group">
-                <label>
-                    The task that should be completed
-                    <input id="taskInputField" type="text" className="form-control" value={command} onChange={(e) => setCommand(e.target.value)} />
-                </label>
-            </div>
-            <div className="form-group">
-                <label>
-                    The context that should be used
-                    <textarea id="contextInputField" className="form-control" value={context} onChange={(e) => setContext(e.target.value)} />
-                </label>
-            </div>
-            <div className="form-group">
-                <label>
-                    <input id="createAVATests" type="checkbox" onChange={e => setAddTests(e.target.checked)} />
-                    Create AVA tests
-                </label>
-            </div>
-            <div className="form-group">
-                <label>
-                    <input id="createDocumentation" type="checkbox" onChange={e => setAddDocumentation(e.target.checked)} />
-                    Add documentation
-                </label>
-            </div>
-            <div className="form-group">
-                <label>
-                    <input id="createFastCR" type="checkbox" onChange={e => setFastReviewMode(e.target.checked)} />
-                    Activate fast Code-Review mode
-                </label>
-            </div>
-            <input id="createPrompt" type="submit" className="btn btn-primary" value="Create Prompt" />
-            <div className="form-group">
-                <label>
-                    The output prompt
-                    <textarea id="promptOut" className="form-control" value={out} onChange={(e) => setOut(e.target.value)} />
-                </label>
-            </div>
-        </form>
-    );
-}
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<div className={"input-section"}>
+					<label htmlFor="taskInputField">
+						The task that should be completed
+					</label>
+					<input id="taskInputField" placeholder={"Enter a task"} type="text" className="form-control" value={command}
+					       onChange={(e) => setCommand(e.target.value)}/>
+					<br/>
+					<label htmlFor="contextInputField">
+						The context that should be used
+					</label>
+					<textarea id="contextInputField"  placeholder={"Enter the context of the task"} className="form-control" value={context}
+					          onChange={(e) => setContext(e.target.value)}/>
+					<br/>
+
+                    <div className={"checkbox-section"}>
+                        <div className="checkbox-grid">
+                            <div>
+                                <input type="checkbox" id="createAVATests" onChange={e => setAddTests(e.target.checked)}/>
+                                <label htmlFor="createAVATests">Create AVA tests</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="createDocumentation" onChange={e => setAddDocumentation(e.target.checked)}/>
+                                <label htmlFor="createDocumentation">Create documentation</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="createFastCR" onChange={e => setFastReviewMode(e.target.checked)}/>
+                                <label htmlFor="createFastCR">Create fast CR</label>
+                            </div>
+                        </div>
+                    </div>
+
+
+					<input id="createPrompt" type="submit" className="btn btn-primary" value="Create Prompt"/>
+				</div>
+                <hr/>
+                <hr/>
+				<div className={"output-section"}>
+					<div className="form-group">
+						<label>
+							The output prompt
+							<textarea readOnly={true}  placeholder={"Generated output prompt"} id="promptOut" className="form-control" value={out}
+							          onChange={(e) => setOut(e.target.value)}/>
+						</label>
+					</div>
+				</div>
+			</form>
+
+			<p>
+				TODO: Introduce megaprompt support
+			</p>
+		</div>
+	);
+};
